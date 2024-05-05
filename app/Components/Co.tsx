77649a4e9844d8f812eeaa921841d9ebbd7a696d
Niharika Goulikar <goulikarniharika@gmail.com>
2024-05-05 21:48:48 +0530

    initial commit

---
+++ app/Components/Co.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React  from 'react';
+import { useState,useEffect} from 'react';
+const samplememes = [
+  "Why don't scientists trust atoms? Because they make up everything!",
+  "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them.",
+  "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  "Why don't skeletons fight each other? They don't have the guts.",
+  "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+  "What do you call fake spaghetti? An impasta!",
+  "I'm reading a book on anti-gravity. It's impossible to put down!",
+  "Why did the bicycle fall over? Because it was two-tired!"
+];
+interface MyComponentProps {
+  optionalProp?: string;
+}
+
+
+const DevMemeGenerator :React.FC<MyComponentProps> =({ optionalProp }) => {
+  const [memes,setmemes]=useState(samplememes);
+  const [randomMeme, setRandomMeme] = useState<string | null>(null);
+ useEffect(()=>{
+generateRandomMeme();
+ })
+
+  const generateRandomMeme = () => {
+    const randomIndex = Math.floor(Math.random() * memes.length);
+    setRandomMeme(memes[randomIndex]);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-200">
+      <div>
+      <div className='block text-rose-400 text-4xl font-bold my-10 text-center'>Jokes Generator</div>
+    <div>
+      {randomMeme && (
+        <div className="italic">
+          
+          <p>😆  <span >   {randomMeme}</span></p>
+        </div>
+      )}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default DevMemeGenerator;
